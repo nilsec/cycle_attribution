045c73a6e19eb93926c89713e10e1ff7965e5aa6
nilsec <nils.eckstein@googlemail.com>
2020-04-24 06:13:49 -0400

    Add aux info to training logs

--- train.py
+++ train.py
@@ -58,8 +58,9 @@ if __name__ == '__main__':
 
             if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
                 losses = model.get_current_losses()
+                aux_infos = model.get_current_aux_infos()
                 t_comp = (time.time() - iter_start_time) / opt.batch_size
-                visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
+                visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data, aux_infos=aux_infos)
                 if opt.display_id > 0:
                     visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
 
