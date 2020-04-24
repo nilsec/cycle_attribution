f1634c7dc6f4fe637a67d3e7835031cb91372988
nilsec <nils.eckstein@googlemail.com>
2020-04-24 12:28:32 -0400

    Factor out aux models

--- models/networks.py
+++ models/networks.py
@@ -4,7 +4,7 @@ from torch.nn import init
 import functools
 from torch.optim import lr_scheduler
 import torch.nn.functional as F
-
+from .aux_nets import Vgg2D
 
 ###############################################################################
 # Helper Functions
@@ -392,113 +392,6 @@ def define_AUX(checkpoint_path, aux_net="Vgg2D", input_nc=1, gpu_ids=[]):
     checkpoint = torch.load(checkpoint_path)
     net.load_state_dict(checkpoint['model_state_dict'])
     return net
-
-class Vgg2D(torch.nn.Module):
-    def __init__(
-            self,
-            input_size,
-            input_channels,
-            fmaps=12,
-            downsample_factors=[(2, 2), (2, 2), (2, 2), (2, 2)],
-            output_classes=6):
-
-        self.input_size = input_size
-
-        super(Vgg2D, self).__init__()
-
-        current_fmaps = 1
-        current_size = tuple(input_size)
-
-        features = []
-        for i in range(len(downsample_factors)):
-
-            features += [
-                torch.nn.Conv2d(
-                    current_fmaps,
-                    fmaps,
-                    kernel_size=3,
-                    padding=1),
-                torch.nn.BatchNorm2d(fmaps),
-                torch.nn.ReLU(inplace=True),
-                torch.nn.Conv2d(
-                    fmaps,
-                    fmaps,
-                    kernel_size=3,
-                    padding=1),
-                torch.nn.BatchNorm2d(fmaps),
-                torch.nn.ReLU(inplace=True),
-                torch.nn.MaxPool2d(downsample_factors[i])
-            ]
-
-            current_fmaps = fmaps
-            fmaps *= 2
-
-            size = tuple(
-                int(c/d)
-                for c, d in zip(current_size, downsample_factors[i]))
-            check = (
-                s*d == c
-                for s, d, c in zip(size, downsample_factors[i], current_size))
-            assert all(check), \
-                "Can not downsample %s by chosen downsample factor" % \
-                (current_size,)
-            current_size = size
-
-        self.features = torch.nn.Sequential(*features)
-
-        classifier = [
-            torch.nn.Linear(
-                current_size[0] *
-                current_size[1] *
-                current_fmaps,
-                4096),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.Dropout(),
-            torch.nn.Linear(
-                4096,
-                4096),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.Dropout(),
-            torch.nn.Linear(
-                4096,
-                output_classes)
-        ]
-
-        self.classifier = torch.nn.Sequential(*classifier)
-
-        print(self)
-
-    def crop(self, raw_with_channels, shape):
-        assert(shape[2] == shape[3])
-        if shape[2] != self.input_size[0]:
-            #print("WARNING: Shape missmatch, center crop input image from {} to aux net input size {}".format(shape, self.input_size))
-            assert(shape[2]%2 == 0 and self.input_size[0] % 2 == 0)
-            c0 = int(shape[2]/2 - self.input_size[0]/2)
-            c1 = int(shape[2]/2 + self.input_size[0]/2)
-
-            raw_with_channels = raw_with_channels[:,:,c0:c1,c0:c1]
-        return raw_with_channels
-
-    def normalize(raw):
-        # is taken care of already
-        raw = raw/255.0
-        raw = raw * 2.0 - 1.0
-        return raw
-
-    def forward(self, raw):
-        shape = tuple(raw.shape)
-        raw_with_channels = raw.reshape(
-            shape[0],
-            1,
-            shape[2],
-            shape[3])
-
-        raw_with_channels = self.crop(raw_with_channels, shape)
-
-        f = self.features(raw_with_channels)
-        f = f.view(f.size(0), -1)
-        y = self.classifier(f)
-        return y
 
 class ResnetBlock(nn.Module):
     """Define a Resnet block"""
